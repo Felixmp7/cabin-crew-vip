84264efd58a8f72f3a7ad86621ee1d846ab4aa4a
Felix Pacheco <devfex.19@gmail.com>
2021-08-19 17:30:16 -0400

    Initialize project

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,24 @@
 module.exports = {
-    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    setupFilesAfterEnv: [
-        '<rootDir>/jest-setup.ts',
-    ],
-    transform: {
-        '.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
+    testEnvironment: 'jest-environment-jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+    testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
+    moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/components', '<rootDir>/pages'],
+    moduleNameMapper: {
+        '@components/(.*)': '<rootDir>/components/$1',
+        '@pages/(.*)': '<rootDir>/pages/$1',
+        '\\.(scss|sass|css)$': 'identity-obj-proxy',
     },
-    testEnvironment: 'jsdom',
+    coverageDirectory: 'coverage',
+    collectCoverageFrom: ['./**/*.{js,jsx,ts,tsx}', 'pages/**/*.{js,jsx,ts,tsx}'],
+    coverageThreshold: {
+        global: {
+            branches: 0,
+            functions: 0,
+            lines: 0,
+            statements: 0,
+        },
+    },
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
 };
